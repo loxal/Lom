b48cd6e41c095850532daa454bb22bd540d751b2
Alexander Orlov <alexander.orlov@loxal.net>
2011-08-09 13:35:20 +0200

    new database implementation

--- Android/src/loxal/lom/Wrapper.java
+++ Android/src/loxal/lom/Wrapper.java
@@ -7,9 +7,7 @@ public class Wrapper extends DroidGap {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        super.loadUrl("file:///android_asset/www/index.html");
-//        super.loadUrl("file:///android_asset/main/main.html");
-//        super.loadUrl("file:///android_asset/app/CV/CV-Alexander-Orlov.html");
+        super.loadUrl("file:///android_asset/main/main.html");
     }
 }
     
